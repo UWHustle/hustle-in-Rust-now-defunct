bc02297e42f1f1135aa3dad507c2160b9e1cdde7
gaffneyk <kgaff33@gmail.com>
2018-11-08 12:31:22 -0600

    CLI now calls parser

--- cli/cli.c
+++ cli/cli.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "parser/parser_wrapper.h"
+#include "parser/stringify.h"
 
 #define HUSTLE_VERSION "0.1.0"
 #define BUFFER_SIZE 1024
@@ -14,7 +15,16 @@ int main(int argc, char **argv) {
     while (!feof(stdin)) {
         printf("%s", PROMPT);
         if (fgets(buffer, BUFFER_SIZE, stdin)) {
-            printf("%s", buffer);
+            parse_node *node = parse(buffer);
+            char *quickstep_output = quickstep_stringify(node);
+            printf("%s\n", quickstep_output);
+            free(quickstep_output);
+
+            char *json_output = json_stringify(node);
+            printf("%s", json_output);
+            free(json_output);
+
+            free(node);
         }
     }
 
