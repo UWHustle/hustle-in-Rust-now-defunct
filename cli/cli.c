c90819273196a7d1a16c955be5a45415cebfa7eb
gaffneyk <kgaff33@gmail.com>
2018-11-08 12:24:05 -0600

    Adding CLI and modifying CMake files

---
+++ cli/cli.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "parser/parser_wrapper.h"
+
+#define HUSTLE_VERSION "0.1.0"
+#define BUFFER_SIZE 1024
+#define PROMPT "hustle> "
+
+int main(int argc, char **argv) {
+    char buffer[BUFFER_SIZE];
+
+    printf("Hustle version %s\n", HUSTLE_VERSION);
+
+    while (!feof(stdin)) {
+        printf("%s", PROMPT);
+        if (fgets(buffer, BUFFER_SIZE, stdin)) {
+            printf("%s", buffer);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
