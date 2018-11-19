53341450bd839ea629242747f1b765fbfc8481d3
gaffneyk <kgaff33@gmail.com>
2018-11-19 14:02:18 -0600

    Add example queries to test

--- parser/test/parser_test.c
+++ parser/test/parser_test.c
@@ -4,18 +4,16 @@
 #include "parser/parse_node.h"
 #include "parser/utility/stringify.h"
 
-int main(void) {
-    char *test = "SELECT c FROM test;";
-    parse_node *node = get_parse_tree(test);
-
-    char *quickstep_output = quickstep_stringify(node);
-    printf("%s\n", quickstep_output);
-    free(quickstep_output);
-
+void test_parse_tree(char *query) {
+    parse_node *node = get_parse_tree(query);
     char *json_output = json_stringify(node);
-    printf("%s", json_output);
+    printf("%s\n", json_output);
     free(json_output);
-
     free_tree(node);
+}
+
+int main(void) {
+    test_parse_tree("SELECT Count(a), b FROM integer_table GROUP BY b;");
+    test_parse_tree("SELECT CommonPrefix(ip) FROM ip_table;");
     return 0;
 }
