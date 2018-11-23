bef6d9184e810a909dcb12fa30a3ed5bd461e25b
yannis <yannis@node.yannis.quickstep-pg0.wisc.cloudlab.us>
2018-11-22 17:52:51 -0700

    query is parser by the optimizer

---
+++ optimizer/optimizer_wrapper.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "./optimizer_wrapper.hpp"
+#include "quickstep/query_optimizer/HustleOptimizer.hpp"
+using namespace std;
+
+
+extern "C" void execute_plan(char*);
+
+extern "C" {
+int optimizer(char *input) {
+  std::cout << "Optimizer input: " << input << std::endl;
+
+  std::string pplan = hustle_optimize(input);
+  char *pplan_char = new char[pplan.size() + 1];
+  std::copy(pplan.begin(), pplan.end(), pplan_char);
+  pplan_char[pplan.size()] = '\0';
+
+
+  std::cout << "Optimizer out: " << pplan << std::endl;
+  execute_plan(pplan_char);
+
+  return 0;
+}
+}
---
+++ optimizer/optimizer_wrapper.hpp
@@ -0,0 +1,11 @@
+#ifndef HUSTLE_OPTIMIZER_OPTIMIZER_WRAPPER_H
+#define HUSTLE_OPTIMIZER_OPTIMIZER_WRAPPER_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+int optimizer(char *input);
+#ifdef __cplusplus
+}
+#endif
+
+#endif //HUSTLE_OPTIMIZER_OPTIMIZER_WRAPPER_H
