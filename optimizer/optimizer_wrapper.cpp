ebc79e417ba53ccd468eb1628489bceb3af8b569
yannis <yannis@node.yannis.quickstep-pg0.wisc.cloudlab.us>
2018-11-27 19:47:52 -0700

    remove debug messages

--- optimizer/optimizer_wrapper.cpp
+++ optimizer/optimizer_wrapper.cpp
@@ -8,17 +8,11 @@ extern "C" void execute_plan(char*);
 
 extern "C" {
 int optimizer(char *input) {
-  //std::cout << "Optimizer input: " << input << std::endl;
-
   std::string pplan = hustle_optimize(input);
   char *pplan_char = new char[pplan.size() + 1];
   std::copy(pplan.begin(), pplan.end(), pplan_char);
   pplan_char[pplan.size()] = '\0';
-
-
-  //std::cout << "Optimizer out: " << pplan << std::endl;
   execute_plan(pplan_char);
-
   return 0;
 }
 }
