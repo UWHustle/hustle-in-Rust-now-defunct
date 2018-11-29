1fee59f59c84a1a7366643b7cffee15a7205d6b1
yannis <yannis@node.yannis.quickstep-pg0.wisc.cloudlab.us>
2018-11-29 12:12:23 -0700

    reduce dependencies to be built

--- optimizer/optimizer_wrapper.cpp
+++ optimizer/optimizer_wrapper.cpp
@@ -12,6 +12,7 @@ int optimizer(char *input) {
   char *pplan_char = new char[pplan.size() + 1];
   std::copy(pplan.begin(), pplan.end(), pplan_char);
   pplan_char[pplan.size()] = '\0';
+  std::cout<< pplan << std::endl;
   execute_plan(pplan_char);
   return 0;
 }
