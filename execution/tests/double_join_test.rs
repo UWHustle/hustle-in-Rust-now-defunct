a4c940d42ce2f29b145cbbbbf667c5b8506340c7
Yannis Chronis <chronis@cs.wisc.edu>
2018-12-30 09:00:45 +0200

    fixed double join parser test

--- execution/tests/double_join_test.rs
+++ execution/tests/double_join_test.rs
@@ -18,17 +18,16 @@ const RECORD_COUNT: usize = 32;
 use execution::physical_plan::node::Node;
 use std::rc::Rc;
 
-// fn test_dag_double_join() {
+#[test]
+fn test_dag_double_join() {
+ let relation = generate_relation_into_hustle_and_sqlite3(RECORD_COUNT);
 
+ let join_relation = hustle_double_join(relation.clone(), relation.clone(), relation.clone());
+ let hustle_calculation = sum_column_hustle(join_relation.clone(), "b".to_string());
+ let sqlite3_calculation = run_query_sqlite3("SELECT SUM(t1.b)+SUM(t2.b)+SUM(t3.b) AS Out FROM t as t1 JOIN t as t2 JOIN t as t3;","Out");
+ assert_eq!(hustle_calculation, sqlite3_calculation);
 
-//     let relation = generate_relation_into_hustle_and_sqlite3(RECORD_COUNT);
-
-//     let join_relation = hustle_double_join(relation.clone(), relation.clone(), relation.clone());
-//     let hustle_calculation = sum_column_hustle(join_relation.clone(), "b".to_string());
-//     let sqlite3_calculation = run_query_sqlite3("SELECT SUM(t1.b)+SUM(t2.b)+SUM(t3.b) AS Out FROM t as t1 JOIN t as t2 JOIN t as t3;","Out");
-//     assert_eq!(hustle_calculation, sqlite3_calculation);
-
-// }
+}
 
 fn sum_column_hustle(relation: Relation, column_name: String) -> u128 {
     let select_operator = SelectSum::new(relation.clone(),Column::new(column_name, "Int".to_string()));
