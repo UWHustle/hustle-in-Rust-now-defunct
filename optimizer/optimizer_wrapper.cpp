53c53a03b58eabc2d611e0e9d426f317132f6f5f
Yannis Chronis <johnchronis@gmail.com>
2019-01-22 12:38:14 -0600

    Quickstep optimizer fails gracefully (#30)
    * Optimizer does not throw a segmentation fault when the quickstep optimizer fails

--- optimizer/optimizer_wrapper.cpp
+++ optimizer/optimizer_wrapper.cpp
@@ -8,6 +8,8 @@ extern "C" void execute_plan(char*);
 
 int optimizer(const shared_ptr<ParseNode> &syntax_tree, const string &sql) {
   std::string pplan = hustle_optimize(syntax_tree, sql);
+  if (pplan.size() == 0) { return -1; } // Quickstep optimizer failed
+
   char *pplan_char = new char[pplan.size() + 1];
   std::copy(pplan.begin(), pplan.end(), pplan_char);
   pplan_char[pplan.size()] = '\0';
