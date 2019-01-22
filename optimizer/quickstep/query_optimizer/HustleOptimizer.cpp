fae140ace82787783cb5733a807a7d72078f9200
Yannis Chronis <johnchronis@gmail.com>
2019-01-22 15:47:13 -0600

    Quickstep parser fails gracefully (#31)
    * Add dependencies for hustle resolver
    
    * Small improvements on the init.sh for linux
    
    * Cmake runs init.sh
    
    * Update CMakeLists.txt
    
    * Optimizer does not throw a segmentation fault when the quickstep optimizer fails
    
    * Quickstep parser fails gracefully
    
    * nit
    
    * nit

--- optimizer/quickstep/query_optimizer/HustleOptimizer.cpp
+++ optimizer/quickstep/query_optimizer/HustleOptimizer.cpp
@@ -7,9 +7,8 @@
 #include "query_optimizer/tests/TestDatabaseLoader.hpp"
 #include "utility/SqlError.hpp"
 
-
-std::string hustle_optimize(const std::shared_ptr<ParseNode> &syntax_tree, const std::string &sql) {
-
+std::string hustle_optimize(const std::shared_ptr<ParseNode> &syntax_tree,
+                            const std::string &sql) {
 
   quickstep::optimizer::OptimizerContext optimizer_context;
 
@@ -20,31 +19,37 @@ std::string hustle_optimize(const std::shared_ptr<ParseNode> &syntax_tree, const
   test_database_loader_.createHustleJoinRelations();
 
   quickstep::optimizer::LogicalGenerator logical_generator(&optimizer_context);
-  quickstep::optimizer::PhysicalGenerator physical_generator(&optimizer_context);
+  quickstep::optimizer::PhysicalGenerator
+      physical_generator(&optimizer_context);
 
   quickstep::optimizer::physical::PhysicalPtr pplan;
   if (sql.empty()) {
     pplan =
-            physical_generator.generatePlan(
-                    logical_generator.hustleGeneratePlan(*test_database_loader_.catalog_database(), syntax_tree),
-                    test_database_loader_.catalog_database());
+        physical_generator.generatePlan(
+            logical_generator.hustleGeneratePlan(
+                *test_database_loader_.catalog_database(), syntax_tree),
+            test_database_loader_.catalog_database());
   } else {
+    quickstep::SqlParserWrapper sql_parser_;
+    auto query = new std::string(sql);
+    sql_parser_.feedNextBuffer(query);
+    quickstep::ParseResult result = sql_parser_.getNextStatement();
+    if (result.condition == quickstep::ParseResult::kError) {
+      printf("%s", result.error_message.c_str());
+      return "";
+    }
+    const quickstep::ParseStatement
+        &parse_statement = *result.parsed_statement;
     try {
-      quickstep::SqlParserWrapper sql_parser_;
-      auto query = new std::string(sql);
-      sql_parser_.feedNextBuffer(query);
-      quickstep::ParseResult result = sql_parser_.getNextStatement();
-      const quickstep::ParseStatement &parse_statement = *result.parsed_statement;
-
       pplan =
-              physical_generator.generatePlan(
-                      logical_generator.generatePlan(*test_database_loader_.catalog_database(), parse_statement),
-                      test_database_loader_.catalog_database());
+          physical_generator.generatePlan(
+              logical_generator.generatePlan(*test_database_loader_.catalog_database(),
+                                             parse_statement),
+              test_database_loader_.catalog_database());
     } catch (const quickstep::SqlError &sql_error) {
       printf("%s", sql_error.formatMessage(sql).c_str());
       return "";
     }
   }
-
   return pplan->jsonString();
 }
