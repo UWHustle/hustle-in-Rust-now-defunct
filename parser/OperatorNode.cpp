6e0396e6eceb1aa194c0d0fe80b4ae2ed4693827
gaffneyk <kgaff33@gmail.com>
2019-01-22 16:25:20 -0600

    Created operator parse node

---
+++ parser/OperatorNode.cpp
@@ -0,0 +1,20 @@
+#include <string>
+
+#include "OperatorNode.h"
+
+using namespace std;
+
+OperatorNode::OperatorNode(OperatorType operator_type, vector<shared_ptr<ParseNode>> operands)
+        : operator_type(operator_type), operands(operands) { }
+
+unordered_map<string, string> OperatorNode::get_attributes() {
+    auto attributes = ParseNode::get_attributes();
+    attributes.insert({"operator", to_string(operator_type)});
+    return attributes;
+}
+
+unordered_map<string, vector<shared_ptr<ParseNode>>> OperatorNode::get_children_lists() {
+    return {
+            {"operands", operands}
+    };
+}
