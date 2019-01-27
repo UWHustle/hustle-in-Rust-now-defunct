be9fb52e400954c607aa8234c31b56256ff020d3
Matthew Dutson <mattdutson@icloud.com>
2019-01-27 13:34:14 -0600

    Fixed projection output column ordering

--- execution/src/physical_operators/project.rs
+++ execution/src/physical_operators/project.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use logical_entities::relation::Relation;
 use logical_entities::column::Column;
 use logical_entities::schema::Schema;
@@ -6,7 +8,7 @@ use storage_manager::StorageManager;
 
 use physical_operators::Operator;
 
-pub const CHUNK_SIZE:usize = 1024*1024;
+pub const CHUNK_SIZE: usize = 1024 * 1024;
 
 #[derive(Debug)]
 pub struct Project {
@@ -18,21 +20,9 @@ pub struct Project {
 }
 
 impl Project {
-    pub fn new(relation: Relation, output_columns:Vec<Column>, predicate_name: String, comparator: i8, comp_value: Vec<u8>) -> Project {
-        /* Re-order the output columns to match the order of the input columns; otherwise values can
-         * be placed in the wrong places. Since we would ideally want to leave the output column
-         * order unchanged this is a temporary fix. */
-        let mut reordered_output_cols: Vec<Column> = vec!();
-        for in_column in relation.get_columns() {
-            for out_column in output_columns.clone() {
-                if in_column.get_name() == out_column.get_name() {
-                    reordered_output_cols.push(out_column.clone());
-                    break;
-                }
-            }
-        }
+    pub fn new(relation: Relation, output_columns: Vec<Column>, predicate_name: String, comparator: i8, comp_value: Vec<u8>) -> Project {
 
-        let schema = Schema::new(reordered_output_cols);
+        let schema = Schema::new(output_columns);
         let output_relation = Relation::new(format!("{}{}", relation.get_name(), "_project".to_string()), schema);
         Project {
             relation,
@@ -49,34 +39,33 @@ impl Operator for Project {
         self.output_relation.clone()
     }
 
-    fn execute(&self) -> Relation{
+    fn execute(&self) -> Relation {
         let mut output_data = StorageManager::create_relation(&self.output_relation, self.relation.get_total_size());
 
-        let input_columns:Vec<Column> = self.relation.get_columns().to_vec();
-        let output_columns:Vec<Column> = self.output_relation.get_columns().to_vec();
+        let input_columns: Vec<Column> = self.relation.get_columns().to_vec();
+        let output_columns: Vec<Column> = self.output_relation.get_columns().to_vec();
 
         let input_data = StorageManager::get_full_data(&self.relation.clone());
 
         let mut i = 0;
-        let mut k = 0;
-        let mut j = 0;
+        let mut k = 0; // Current position in the input relation
+        let mut j = 0; // Current position in the output relation
 
         while i < input_data.len() {
             let mut filter: bool = true;
-            if self.comparator >= (-1) && self.comparator <=1 {
+            if self.comparator >= (-1) && self.comparator <= 1 {
                 for column in &input_columns {
                     let value_length = column.get_datatype().get_next_length(&input_data[k..]);
 
-                    if  column.get_name().to_string() == self.predicate_name {
-                           //let mut s:Vec<u8> = Vec::new();
-                           //let s = column.get_datatype().sum(&s, &input_data[i..i + value_length].to_vec()).0;
-                           let value = &input_data[k..k + value_length].to_vec();
-                           if !( column.get_datatype().compare(value, &self.comp_value) == self.comparator){
-                                filter=false;
-
-                           }
-                           //if !value.std::cmp::Eq(self.comp_value){
-                           //   filter = false;
+                    if column.get_name().to_string() == self.predicate_name {
+                        //let mut s:Vec<u8> = Vec::new();
+                        //let s = column.get_datatype().sum(&s, &input_data[i..i + value_length].to_vec()).0;
+                        let value = &input_data[k..k + value_length].to_vec();
+                        if !(column.get_datatype().compare(value, &self.comp_value) == self.comparator) {
+                            filter = false;
+                        }
+                        //if !value.std::cmp::Eq(self.comp_value){
+                        //   filter = false;
 
                         //}
                     }
@@ -84,19 +73,22 @@ impl Operator for Project {
                 }
             }
             if filter {
-            k=i;
-            for column in &input_columns {
-                let value_length = column.get_datatype().get_next_length(&input_data[k..]);
+                let mut column_map = HashMap::new();
 
-                if (&output_columns).into_iter().any(|x| { x.get_name() == column.get_name()}) {
-                    output_data[j..j + value_length].clone_from_slice(&input_data[k..k + value_length]);
-                    j += value_length;
-
+                k = i;
+                for column in &input_columns {
+                    let value_length = column.get_datatype().get_next_length(&input_data[k..]);
+                    column_map.insert(column, &input_data[k..k + value_length]);
+                    k += value_length;
                 }
-                k += value_length;
 
+                for column in &output_columns {
+                    let slice = column_map[column];
+                    let value_length = column.get_datatype().get_next_length(slice);
+                    output_data[j..j + value_length].clone_from_slice(slice);
+                    j += value_length;
+                }
             }
-          }
             i = k;
         }
 
