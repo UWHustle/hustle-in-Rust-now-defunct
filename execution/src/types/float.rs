3cf79025030b14bf9c8f78bb9576b583abb07315
srora <somyarora96@gmail.com>
2019-02-01 13:52:27 -0600

    adding float4

--- execution/src/types/float.rs
+++ execution/src/types/float.rs
@@ -39,13 +39,68 @@ impl ValueType for Float8 {
             },
             TypeID::Int8 => {
                 self.value == cast::<Int8>(other).value() as f64
-            }
+            },
             TypeID::Float8 => {
                 self.value == cast::<Float8>(other).value
             },
             TypeID::IPv4 => {
                 self.value == cast::<IPv4>(other).value() as f64
+            },
+            _ => {
+                panic!(incompatible_types(self.type_id(), other.type_id()));
+            },
+        }
+    }
+    fn type_id(&self) -> TypeID {
+        TypeID::Float8
+    }
+}
+
+pub struct Float4 {
+    value: f32
+}
+
+impl Float4 {
+    pub fn new(data: &[u8]) -> Self {
+        Float4 { value: LittleEndian::read_f32(&data) }
+    }
+    pub fn value(&self) -> f32 {
+        self.value
+    }
+    fn cmp(&self, other:f32) {
+        
+    }
+}
+
+impl ValueType for Float4 {
+    fn un_marshall(&self) -> OwnedBuffer {
+        let mut data: Vec<u8> = vec![0; 4];
+        LittleEndian::write_f32(&mut data, self.value);
+        OwnedBuffer::new(self.type_id(), data)
+    }
+
+    fn size(&self) -> usize {4}
+
+    fn equals(&self, other: &ValueType) -> bool {
+        match other.type_id() {
+            TypeID::Int2 => {
+                self.value == cast::<Int2>(other).value() as f32
+            },
+            TypeID::Int4 => {
+                self.value == cast::<Int4>(other).value() as f32
+            },
+            TypeID::Int8 => {
+                self.value == cast::<Int8>(other).value() as f32
+            },
+            TypeID::Float4 => {
+                self.value == cast::<Float4>(other).value
+            },
+            TypeID::Float8 => {
+                self.value ==cast::<Float8>(other).value as f32
             }
+            TypeID::IPv4 => {
+                self.value == cast::<IPv4>(other).value() as f32
+            },
             _ => {
                 panic!(incompatible_types(self.type_id(), other.type_id()));
             },
