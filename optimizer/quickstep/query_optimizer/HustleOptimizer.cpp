8d41f9553f8e29fd7007e78347cb0683489cfbc1
Yannis Chronis <chronis@cs.wisc.edu>
2019-02-05 21:27:24 -0600

    One create the physical plan in one place

--- optimizer/quickstep/query_optimizer/HustleOptimizer.cpp
+++ optimizer/quickstep/query_optimizer/HustleOptimizer.cpp
@@ -19,7 +19,7 @@ std::string hustle_optimize(const std::shared_ptr<ParseNode> &syntax_tree,
 
   quickstep::optimizer::LogicalGenerator logical_generator(&optimizer_context);
   quickstep::optimizer::PhysicalGenerator
-                            physical_generator(&optimizer_context);
+      physical_generator(&optimizer_context);
 
   quickstep::optimizer::physical::PhysicalPtr pplan;
   try {
@@ -27,7 +27,7 @@ std::string hustle_optimize(const std::shared_ptr<ParseNode> &syntax_tree,
     // If the sql is empty the parser/resolver of Hustle is used.
     if (sql.empty()) {
       lplan = logical_generator.hustleGeneratePlan(
-                      *test_database_loader_.catalog_database(), syntax_tree);
+          *test_database_loader_.catalog_database(), syntax_tree);
     } else {  // Parse the query using the quickstep parser
       quickstep::SqlParserWrapper sql_parser_;
       auto query = new std::string(sql);
@@ -41,14 +41,14 @@ std::string hustle_optimize(const std::shared_ptr<ParseNode> &syntax_tree,
           &parse_statement = *result.parsed_statement;
       // Convert the query to the logical plan using quickstep's optimizer
       lplan = logical_generator.generatePlan(
-              *test_database_loader_.catalog_database(), parse_statement);
+          *test_database_loader_.catalog_database(), parse_statement);
     }
 
     std::cout<< "Logical Plan: " << lplan->jsonString() << std::endl;
     std::cout << " --------------------- " << std::endl;
     pplan =
-      physical_generator.generatePlan(
-          lplan, test_database_loader_.catalog_database());
+        physical_generator.generatePlan(
+            lplan, test_database_loader_.catalog_database());
 
     std::cout<< "Physical Plan: " << pplan->jsonString() << std::endl;
   } catch (const quickstep::SqlError &sql_error) {
