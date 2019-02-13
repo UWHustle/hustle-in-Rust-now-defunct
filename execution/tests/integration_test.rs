d863a9d0be8558f6a13ae051ac05564602f7630b
Matthew Dutson <mattdutson@icloud.com>
2019-02-13 10:57:00 -0600

    Fixed integration test compiler errors

--- execution/tests/integration_test.rs
+++ execution/tests/integration_test.rs
@@ -3,52 +3,44 @@ extern crate execution;
 use execution::test_helpers::sqlite3::run_query_sqlite3;
 use execution::test_helpers::data_gen::insert_into_hustle;
 use execution::test_helpers::data_gen::generate_relation_into_hustle_and_sqlite3;
-
-
 use execution::logical_entities::relation::Relation;
 use execution::logical_entities::column::Column;
-
 use execution::physical_operators::join::Join;
 use execution::physical_operators::select_sum::SelectSum;
-use execution::type_system::*;
 use execution::type_system::type_id::*;
-
+use execution::type_system::integer::*;
 use execution::physical_operators::Operator;
 
 extern crate csv;
 
-
 const RECORD_COUNT: usize = 1024;
-
 
 #[test]
 fn test_flow() {
-//    let relation = generate_relation_into_hustle_and_sqlite3(RECORD_COUNT);
-//
-//    let hustle_calculation = sum_column_hustle(relation.clone(), "b".to_string());
-//    let sqlite3_calculation = run_query_sqlite3("SELECT SUM(b) FROM T;", "SUM(b)");
-//    assert_eq!(hustle_calculation, sqlite3_calculation);
-//
-//
-//    let join_relation = hustle_join(relation.clone(), relation.clone());
-//    let hustle_calculation = sum_column_hustle(join_relation.clone(), "b".to_string());
-//    let sqlite3_calculation = run_query_sqlite3("SELECT SUM(t1.b)+SUM(t2.b) as Out FROM t as t1 JOIN t as t2;", "Out");
-//    assert_eq!(hustle_calculation, sqlite3_calculation);
-//
-//
-//    let insert_value = DataType::Integer.parse_to_value("3".to_string());
-//    insert_into_hustle(10, insert_value,  relation.clone());
-//    let hustle_calculation = sum_column_hustle(relation.clone(), "b".to_string());
-//    let sqlite3_calculation = run_query_sqlite3("SELECT SUM(b) FROM T;","SUM(b)");
-//    assert_eq!(hustle_calculation, sqlite3_calculation+30);
+    let relation = generate_relation_into_hustle_and_sqlite3(RECORD_COUNT);
+
+    let hustle_calculation = sum_column_hustle(relation.clone(), "b".to_string());
+    let sqlite3_calculation = run_query_sqlite3("SELECT SUM(b) FROM T;", "SUM(b)");
+    assert_eq!(hustle_calculation, sqlite3_calculation);
+
+    let join_relation = hustle_join(relation.clone(), relation.clone());
+    let hustle_calculation = sum_column_hustle(join_relation.clone(), "b".to_string());
+    let sqlite3_calculation = run_query_sqlite3("SELECT SUM(t1.b)+SUM(t2.b) as Out FROM t as t1 JOIN t as t2;", "Out");
+    assert_eq!(hustle_calculation, sqlite3_calculation);
+
+    let insert_value = Int4::new(3);
+    insert_into_hustle(10, &insert_value, relation.clone());
+    let hustle_calculation = sum_column_hustle(relation.clone(), "b".to_string());
+    let sqlite3_calculation = run_query_sqlite3("SELECT SUM(b) FROM T;", "SUM(b)");
+    assert_eq!(hustle_calculation, sqlite3_calculation + 30);
 }
 
 fn sum_column_hustle(relation: Relation, column_name: String) -> u128 {
-    let select_operator = SelectSum::new(relation.clone(),Column::new(column_name, TypeID::new(Variant::Int4, true)));
+    let select_operator = SelectSum::new(relation.clone(), Column::new(column_name, TypeID::new(Variant::Int4, true)));
     select_operator.execute().parse::<u128>().unwrap()
 }
 
-fn hustle_join(relation1:Relation, relation2:Relation) -> Relation {
+fn hustle_join(relation1: Relation, relation2: Relation) -> Relation {
     let join_operator = Join::new(relation1.clone(), relation2.clone());
     join_operator.execute()
 }
--- execution/tests/sum_test.rs
+++ execution/tests/sum_test.rs
@@ -9,29 +9,30 @@ use execution::physical_operators::select_sum::SelectSum;
 use execution::physical_plan::node::Node;
 use execution::test_helpers::data_gen::generate_relation_into_hustle_and_sqlite3;
 use execution::test_helpers::sqlite3::run_query_sqlite3;
+use execution::type_system::type_id::*;
 
 use std::rc::Rc;
 
 const RECORD_COUNT: usize = 10;
 
-//#[test]
-//fn test_sum_aggregate() {
-//    let relation = generate_relation_into_hustle_and_sqlite3(RECORD_COUNT);
-//    let agg_col = Column::new(String::from("a"), String::from("Int"));
-//    let agg_relation = hustle_sum(relation.clone(), agg_col);
-//    let hustle_calculation = sum_column_hustle(agg_relation.clone(), "SUM(a)".to_string());
-//    let sqlite3_calculation = run_query_sqlite3("SELECT SUM(t.a) FROM t;", "SUM(t.a)");
-//    assert_eq!(hustle_calculation, sqlite3_calculation);
-//}
-//
-//fn sum_column_hustle(relation: Relation, column_name: String) -> u128 {
-//    let select_operator = SelectSum::new(relation.clone(), Column::new(column_name, "Int".to_string()));
-//    select_operator.execute().parse::<u128>().unwrap()
-//}
-//
-//fn hustle_sum(relation: Relation, agg_col: Column) -> Relation {
-//    let project_op = Project::pure_project(relation, vec!(agg_col.clone()));
-//    let project_node = Node::new(Rc::new(project_op), vec!());
-//    let agg_op = Aggregate::new(project_node.get_output_relation(), agg_col.clone(), vec!(), Sum::new(agg_col.get_datatype()));
-//    Node::new(Rc::new(agg_op), vec!(Rc::new(project_node))).execute()
-//}
+#[test]
+fn test_sum_aggregate() {
+    let relation = generate_relation_into_hustle_and_sqlite3(RECORD_COUNT);
+    let agg_col = Column::new(String::from("a"), TypeID::new(Variant::Int4, true));
+    let agg_relation = hustle_sum(relation.clone(), agg_col);
+    let hustle_calculation = sum_column_hustle(agg_relation.clone(), "SUM(a)".to_string());
+    let sqlite3_calculation = run_query_sqlite3("SELECT SUM(t.a) FROM t;", "SUM(t.a)");
+    assert_eq!(hustle_calculation, sqlite3_calculation);
+}
+
+fn sum_column_hustle(relation: Relation, column_name: String) -> u128 {
+    let select_operator = SelectSum::new(relation.clone(), Column::new(column_name, TypeID::new(Variant::Int4, true)));
+    select_operator.execute().parse::<u128>().unwrap()
+}
+
+fn hustle_sum(relation: Relation, agg_col: Column) -> Relation {
+    let project_op = Project::pure_project(relation, vec!(agg_col.clone()));
+    let project_node = Node::new(Rc::new(project_op), vec!());
+    let agg_op = Aggregate::new(project_node.get_output_relation(), agg_col.clone(), vec!(), Sum::new(agg_col.get_datatype()));
+    Node::new(Rc::new(agg_op), vec!(Rc::new(project_node))).execute()
+}
