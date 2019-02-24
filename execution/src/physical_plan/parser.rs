56bde9f853eec9e2008f686ac764c3e811fd1bee
Matthew Dutson <mattdutson@icloud.com>
2019-02-24 16:12:05 -0600

    Execution CREATE TABLE support (#56)
    * Execute create table
    
    * create table support
    
    * Fixed some CREATE TABLE bugs
    
    * Working CREATE TABLE
    
    The execution engine's CreateTable operator is currently just a
    pass-through because memmap doesn't like zero-length maps.

--- execution/src/physical_plan/parser.rs
+++ execution/src/physical_plan/parser.rs
@@ -11,6 +11,7 @@ use physical_operators::join::Join;
 use physical_operators::limit::Limit;
 use physical_operators::print::Print;
 use physical_operators::project::Project;
+use physical_operators::create_table::CreateTable;
 use physical_operators::table_reference::TableReference;
 use physical_plan::node::Node;
 use type_system::operators::*;
@@ -24,10 +25,16 @@ use std::rc::Rc;
 
 pub fn parse(string_plan: &str) -> Node {
     let json: Value = serde_json::from_str(string_plan).unwrap();
-
     let root_node = parse_node(&json["plan"]);
-    let print_op = Print::new(root_node.get_output_relation());
-    Node::new(Rc::new(print_op), vec![Rc::new(root_node)])
+
+    // We only want to print for a selection
+    if &json["plan"]["json_name"] == "Selection" {
+        let print_op = Print::new(root_node.get_output_relation());
+        Node::new(Rc::new(print_op), vec![Rc::new(root_node)])
+    } else {
+        root_node
+    }
+
 }
 
 fn parse_node(json: &Value) -> Node {
@@ -38,6 +45,7 @@ fn parse_node(json: &Value) -> Node {
         "Aggregate" => parse_aggregate(json),
         "HashJoin" => parse_hash_join(json),
         "Limit" => parse_limit(json),
+        "CreateTable" => parse_create_table(json),
         _ => panic!("Optimizer tree node type {} not supported", json_name),
     }
 }
@@ -165,6 +173,12 @@ fn parse_selection(json: &Value) -> Node {
         }
     };
     Node::new(Rc::new(project_op), vec![Rc::new(input)])
+}
+
+fn parse_create_table(json: &Value) -> Node {
+    let cols = parse_column_list(&json["attributes"]);
+    let relation = Relation::new(get_string(&json["relation"]), Schema::new(cols));
+    Node::new(Rc::new(CreateTable::new(relation)), vec![])
 }
 
 fn parse_table_reference(json: &Value) -> Node {
