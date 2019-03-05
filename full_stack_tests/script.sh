2decc2cf27ec992cf44ffacf57f70e9312e58cd1
srora <somyarora96@gmail.com>
2019-03-04 18:31:23 -0600

    full stack testing

--- full_stack_tests/script.sh
+++ full_stack_tests/script.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+set -x
 rm -f test_output.txt
 cd ../build
-rm -f test-data/test_table.hsl
+rm -f ./test-data/test_table.hsl
+rm -f ./test-data/test_table_project.hsl
+
 OUT="../full_stack_tests/test_output.txt"
 CMD="../full_stack_tests/end_to_end_tests.txt"
+CMP="../full_stack_tests/compare_output.txt"
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
-	echo "$line" >> test_output.txt
-	./hustle <<<$line >> $OUT 2>&1
+echo "$line" >> $OUT
+./hustle <<<$line >> $OUT 2>&1
 done < $CMD
+
+DIFF=$(diff $OUT $CMP)
+echo "$DIFF"
+echo "$DIFF" >> diff.txt
