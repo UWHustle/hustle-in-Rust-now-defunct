8a82312a607af581531d678f5301d5aee92d4797
Matthew Dutson <mattdutson@icloud.com>
2019-03-27 12:45:07 -0500

    Text file containing sample TATP queries

---
+++ tatp_queries.sql
@@ -0,0 +1,71 @@
+-- noinspection SqlDialectInspectionForFile
+-- noinspection SqlNoDataSourceInspectionForFile
+
+
+-- Creation commands
+CREATE TABLE Subscriber (s_id INTEGER NOT NULL PRIMARY KEY, sub_nbr VARCHAR(15) NOT NULL UNIQUE, bit_1 TINYINT, bit_2 TINYINT, bit_3 TINYINT, bit_4 TINYINT, bit_5 TINYINT, bit_6 TINYINT, bit_7 TINYINT, bit_8 TINYINT, bit_9 TINYINT, bit_10 TINYINT, hex_1 TINYINT, hex_2 TINYINT, hex_3 TINYINT, hex_4 TINYINT, hex_5 TINYINT, hex_6 TINYINT, hex_7 TINYINT, hex_8 TINYINT, hex_9 TINYINT, hex_10 TINYINT, byte2_1 SMALLINT, byte2_2 SMALLINT, byte2_3 SMALLINT, byte2_4 SMALLINT, byte2_5 SMALLINT, byte2_6 SMALLINT, byte2_7 SMALLINT, byte2_8 SMALLINT, byte2_9 SMALLINT, byte2_10 SMALLINT, msc_location INTEGER, vlr_location INTEGER);
+CREATE TABLE Access_Info (s_id INTEGER NOT NULL, ai_type TINYINT NOT NULL, data1 SMALLINT, data2 SMALLINT, data3 CHAR(3), data4 CHAR(5), PRIMARY KEY(s_id, ai_type), FOREIGN KEY (s_id) REFERENCES Subscriber (s_id));
+CREATE TABLE Special_Facility (s_id INTEGER NOT NULL, sf_type TINYINT NOT NULL, is_active TINYINT NOT NULL, error_cntrl SMALLINT, data_a SMALLINT, data_b CHAR(5), PRIMARY KEY (s_id, sf_type), FOREIGN KEY (s_id) REFERENCES Subscriber (s_id));
+CREATE TABLE Call_Forwarding (s_id INTEGER NOT NULL, sf_type TINYINT NOT NULL, start_time TINYINT NOT NULL, end_time TINYINT, numberx VARCHAR(15), PRIMARY KEY (s_id, sf_type, start_time), FOREIGN KEY (s_id, sf_type) REFERENCES Special_Facility(s_id, sf_type));
+
+
+-- TODO: Population commands
+
+
+-- Selection commands
+
+/*
+ * GET_SUBSCRIBER_DATA
+ *  - <s_id> is randomly selected from [1, size of Subscriber table]
+ *  - The probability for the transaction to succeed is 100%
+ */
+SELECT s_id, sub_nbr, bit_1, bit_2, bit_3, bit_4, bit_5, bit_6, bit_7, bit_8, bit_9, bit_10, hex_1, hex_2, hex_3, hex_4, hex_5, hex_6, hex_7, hex_8, hex_9, hex_10, byte2_1, byte2_2, byte2_3, byte2_4, byte2_5, byte2_6, byte2_7, byte2_8, byte2_9, byte2_10, msc_location, vlr_location FROM Subscriber WHERE s_id = <s_id>;
+
+/*
+ * GET_NEW_DESTINATION
+ *  - <s_id> is randomly selected from [1, size of Subscriber table]
+ *  - <sf_type> is randomly selected from [1, 4]
+ *  - <start_time> is randomly selected from {0, 8, 16}
+ *  - <end_time> is randomly selected from [1, 24]
+ *  - The probability for the transaction to succeed is 23.9%
+ */
+SELECT cf.numberx FROM Special_Facility AS sf, Call_Forwarding AS cf WHERE (sf.s_id = <s_id> AND sf.sf_type = <sf_type> AND sf.is_active = 1) AND (cf.s_id = sf.s_id AND cf.sf_type = sf.sf_type) AND (cf.start_time <= <start_time> AND <end_time> < cf.end_time);
+
+/*
+ * GET_ACCESS_DATA
+ *  - <s_id> is randomly selected from [1, size of Subscriber table]
+ *  - <ai_type> is randomly selected from [1, 4]
+ *  - The probability for the transaction to succeed is 62.5%
+ */
+SELECT data1, data2, data3, data4 FROM Access_Info WHERE s_id = <s_id> AND ai_type = <ai_type>;
+
+/*
+ * UPDATE_LOCATION
+ *  - vlr_location is randomly selected from [1, 2^32 - 1]
+ *  - <sub_nbr> is the string representation of a random number from [1, size of Subscriber table]
+ *  - The probability for the transaction to succeed is 100%
+ */
+UPDATE Subscriber SET vlr_location = <vlr_location> WHERE sub_nbr = <sub_nbr>;
+
+/*
+ * INSERT_CALL_FORWARDING
+ *  - <sub_nbr> is the string representation of a random number from [1, size of Subscriber table]
+ *  - <sf_type> is randomly selected from [1, 4]
+ *  - <start_time> is randomly selected from {0, 8, 16}
+ *  - <end_time> is randomly selected from [1, 24]
+ *  - <numberx> is a string of length 15 characters. A number between [1, size of Subscriber table] is randomly generated, converted to string representation and padded with the character zero.
+ *  - The probability for the transaction to succeed is 31.25%
+ */
+SELECT <s_id bind subid> FROM Subscriber WHERE sub_nbr = <sub_nbr>;
+SELECT <sf_type bind sfid> FROM Special_Facility WHERE s_id = <subid>;
+INSERT INTO Call_Forwarding VALUES (<s_id>, <sf_type>, <start_time>, <end_time>, <numberx>);
+
+/*
+ * DELETE_CALL_FORWARDING
+ *  - <s_id> is randomly selected from [1, size of Subscriber table]
+ *  - sf_type is randomly selected from [1, 4]
+ *  - start_time is randomly selected from {0, 8, 16}
+ *  - The probability for the transaction to succeed is 31.25%
+ */
+SELECT <s_id bind subid> FROM Subscriber WHERE sub_nbr = <sub_nbr>;
+DELETE FROM Call_Forwarding WHERE s_id = <s_id> AND sf_type = <sf_type> AND start_time = <start_time>;
