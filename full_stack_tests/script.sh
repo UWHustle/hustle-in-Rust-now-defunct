e95db824779013398c8d13432640dedb6ef92644
Matthew Dutson <mattdutson@icloud.com>
2019-04-16 18:33:09 -0500

    Fixing failed full stack testing (test data file was in wrong location)

--- full_stack_tests/script.sh
+++ full_stack_tests/script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -x
-#rm -f ../full_stack_tests/test_output.txt
 cd ../build
 rm -f diff.txt
-rm -f ./test-data/test_table.hsl
-rm -f ./test-data/test_table_project.hsl
 
 OUT="../full_stack_tests/test_output.txt"
 CMD="../full_stack_tests/end_to_end_tests.txt"
@@ -12,7 +9,7 @@ CMP="../full_stack_tests/compare_output.txt"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
 echo "$line" >> $OUT
-sudo ./hustle <<<$line >> $OUT 2>&1
+./hustle <<<$line >> $OUT 2>&1
 done < $CMD
 
 DIFF=$(diff $OUT $CMP)
