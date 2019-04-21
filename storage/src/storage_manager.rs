b34f266ed4b89cc02f311d81cff98c14daac0b1a
gaffneyk <kgaff33@gmail.com>
2019-04-21 17:04:47 -0500

    Added len() for Record

--- storage/src/storage_manager.rs
+++ storage/src/storage_manager.rs
@@ -58,6 +58,21 @@ impl<'a> Record<'a> {
         }
     }
 
+    /// Returns the number of rows in the `Record`.
+    pub fn len(&self) -> usize {
+        let mut tail_block_index = 0;
+        while self.buffer.exists(&StorageManager::key_for_block(key, tail_block_index + 1)) {
+            tail_block_index += 1;
+        }
+
+        let key_for_tail_block = Self::key_for_block(key, tail_block_index);
+        let rows_in_tail_block = self.buffer.get(&key_for_tail_block)
+            .map(|block| block.len() / self.row_size)
+            .unwrap_or(0);
+
+        return tail_block_index * self.rows_per_block + rows_in_tail_block;
+    }
+
     /// Returns an iterator over the record's blocks.
     pub fn blocks(&self) -> RecordBlockIter {
         RecordBlockIter::new(self)
