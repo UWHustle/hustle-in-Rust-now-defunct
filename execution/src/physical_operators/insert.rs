196ff3ab84c7fdae7db98090aeebf649652ff12e
Matthew Dutson <mattdutson@icloud.com>
2019-05-01 16:57:02 -0500

    Rewrite of insert operator

--- execution/src/physical_operators/insert.rs
+++ execution/src/physical_operators/insert.rs
@@ -23,21 +23,11 @@ impl Operator for Insert {
 
     fn execute(&self, storage_manager: &StorageManager) -> Result<Relation, String> {
         let mut data: Vec<u8> = vec![];
-        match storage_manager.get(self.relation.get_name()) {
-            Some(value) => data = value.to_vec(),
-            None => (),
-        }
-
-        let mut n = data.len();
-        data.resize(n + self.row.get_size(), 0);
         for value in self.row.get_values() {
-            let size = value.size();
-            data[n..n + size].clone_from_slice(value.un_marshall().data());
-            n += size;
+            data.extend_from_slice(value.un_marshall().data());
         }
 
-        storage_manager.put(self.relation.get_name(), &data);
-
+        storage_manager.append(self.relation.get_name(), &data);
         Ok(self.get_target_relation())
     }
 }
