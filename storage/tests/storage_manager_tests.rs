af503846ccf5020daa07479b78ebc8969b6fcc95
gaffneyk <kgaff33@gmail.com>
2019-05-13 11:11:10 -0500

    Added delete support in storage manager

--- storage/tests/storage_manager_tests.rs
+++ storage/tests/storage_manager_tests.rs
@@ -48,33 +48,6 @@ mod storage_manager_tests {
     }
 
     #[test]
-    fn insert() {
-        let sm = StorageManager::new();
-        let rl_engine = sm.relational_engine();
-        let pr = rl_engine.create("key_insert", vec![1, 2]);
-
-        {
-            let mut row_builder = pr.insert_row();
-            row_builder.push(b"a");
-            row_builder.push(b"bb");
-        }
-
-        {
-            let mut row_builder = pr.insert_row();
-            row_builder.push(b"c");
-            row_builder.push(b"dd");
-        }
-
-        let block = pr.get_block(0).unwrap();
-        assert_eq!(&block.get_row_col(0, 0).unwrap(), &b"a");
-        assert_eq!(&block.get_row_col(0, 1).unwrap(), &b"bb");
-        assert_eq!(&block.get_row_col(1, 0).unwrap(), &b"c");
-        assert_eq!(&block.get_row_col(1, 1).unwrap(), &b"dd");
-
-        rl_engine.drop("key_insert");
-    }
-
-    #[test]
     fn get_row_col() {
         let sm = StorageManager::new();
         let rl_engine = sm.relational_engine();
@@ -105,6 +78,57 @@ mod storage_manager_tests {
         assert_eq!(&block.bulk_read()[0..6], b"ebbcff");
 
         rl_engine.drop("key_set_row_col");
+    }
+
+    #[test]
+    fn insert_row() {
+        let sm = StorageManager::new();
+        let rl_engine = sm.relational_engine();
+        let pr = rl_engine.create("key_insert_row", vec![1, 2]);
+
+        {
+            let mut row_builder = pr.insert_row();
+            row_builder.push(b"a");
+            row_builder.push(b"bb");
+        }
+
+        {
+            let mut row_builder = pr.insert_row();
+            row_builder.push(b"c");
+            row_builder.push(b"dd");
+        }
+
+        let block = pr.get_block(0).unwrap();
+        assert_eq!(&block.get_row_col(0, 0).unwrap(), &b"a");
+        assert_eq!(&block.get_row_col(0, 1).unwrap(), &b"bb");
+        assert_eq!(&block.get_row_col(1, 0).unwrap(), &b"c");
+        assert_eq!(&block.get_row_col(1, 1).unwrap(), &b"dd");
+
+        rl_engine.drop("key_insert_row");
+    }
+
+    #[test]
+    fn delete_row() {
+        let sm = StorageManager::new();
+        let rl_engine = sm.relational_engine();
+        let pr = rl_engine.create("key_delete_row", vec![1, 2]);
+
+        pr.bulk_write(b"abbcddeff");
+
+        let mut block = pr.get_block(0).unwrap();
+
+        block.delete_row(0);
+        assert_eq!(block.get_n_rows(), 2);
+        assert_eq!(&block.bulk_read(), b"effcdd");
+
+        block.delete_row(1);
+        assert_eq!(block.get_n_rows(), 1);
+        assert_eq!(&block.bulk_read(), b"eff");
+
+        block.delete_row(0);
+        assert_eq!(block.get_n_rows(), 0);
+
+        rl_engine.drop("key_delete_row");
     }
 
     #[test]
