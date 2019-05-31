2fc0de648f1a70674e68fc96f3736641cd15354c
gaffneyk <kgaff33@gmail.com>
2019-05-31 12:05:41 -0500

    Fixed execution tests

--- execution/tests/limit_test.rs
+++ execution/tests/limit_test.rs
@@ -1,23 +1,25 @@
 extern crate execution;
+extern crate storage;
 
 use execution::physical_operators::limit::Limit;
 use execution::physical_operators::Operator;
-use execution::physical_plan::global_sm;
 use execution::test_helpers::generate_data::generate_t_hustle_and_sqlite;
 use execution::test_helpers::hustle_queries::hustle_agg;
 use execution::type_system::data_type::*;
+use storage::StorageManager;
 
 const RECORD_COUNT: usize = 10;
 
 #[test]
 fn test_limit() {
-    let input_relation = generate_t_hustle_and_sqlite(global_sm::get(), RECORD_COUNT, true);
+    let storage_manager = StorageManager::new();
+    let input_relation = generate_t_hustle_and_sqlite(&storage_manager, RECORD_COUNT, true);
     let output_relation = Limit::new(input_relation, 5)
-        .execute(global_sm::get())
+        .execute(&storage_manager)
         .unwrap();
     let n_rows = hustle_agg(
-        global_sm::get(),
-        output_relation,
+        &storage_manager,
+        output_relation.unwrap(),
         "a",
         DataType::new(Variant::Int8, true),
         "count",
