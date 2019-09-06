9d99a51a329a62e0e815adef47c9045c413b0a22
gaffneyk <kgaff33@gmail.com>
2019-09-05 20:44:56 -0500

    Removed old parser

--- parser/build.rs
+++
@@ -1,9 +0,0 @@
-extern crate cmake;
-
-fn main() {
-    let mut dst = cmake::build("parser");
-    dst.push("build");
-
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=hustle_parser");
-}
--- parser/parser/parser_wrapper.h
+++
@@ -1,8 +0,0 @@
-#ifndef HUSTLE_PARSER_PARSER_WRAPPER_H
-#define HUSTLE_PARSER_PARSER_WRAPPER_H
-
-#include "parse_context.h"
-
-int c_parse(char *command, char **result);
-
-#endif //HUSTLE_PARSER_PARSER_WRAPPER_H
