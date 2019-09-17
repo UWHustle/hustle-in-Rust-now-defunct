24bcd0bdcafe00fa32f0d8acfe3a8b6bab17ce02
srora <somyarora96@gmail.com>
2019-09-17 13:52:47 -0500

    hash join test

--- execution/src/operator/hash_join.rs
+++ execution/src/operator/hash_join.rs
@@ -56,17 +56,21 @@ impl Operator for Hash_Join {
 
         for input_block_id in &self.block_rx_table_2 {
             let input_block = storage_manager.get_block(input_block_id).unwrap();
-            let block_with_match = &mut input_block.clone();
+         //   let block_with_match = &mut input_block.clone();
 
+            // transform vector<(row id of table 1, block id of table 1)> to (row id of table 2, vector<(row id of table 1, block id of table 1)>)
             let row_ids = input_block.get_col(self.join_attribute_table_2).unwrap()
                 .filter_map(|(input_row_id, key)|
-                    hash_table.get(key).map(|key| (input_row_id, key))
+                    hash_table.get(key).map(|value_by_hash_table| (input_row_id, value_by_hash_table))
                 );
 
-            for (input_row_id, matched_ids) in row_ids {
-                let matched_row_ids = matched_ids.iter()
+            // transform (input_row_id, vector<(row id, block id)> to (input_row_id, vector<(row id, actual block corresponding to that block id)>)
+            for (input_row_id, value_by_hash_table) in row_ids {
+                let matched_row_ids = value_by_hash_table.iter()
                     .map(|&(row_id, block_id)| (row_id, storage_manager.get_block(block_id).unwrap()))
                     .collect::<Vec<_>>();
+
+                let input_row = input_block.get_row(input_row_id).unwrap();
 
                 let mut rows = matched_row_ids.iter()
                     .map(|(matched_row_id, block_with_match)|
@@ -75,11 +79,16 @@ impl Operator for Hash_Join {
 
                     );
 
-                util::send_rows(&mut rows, &self.block_tx, &self.router, storage_manager);
+                util::send_rows(&mut rows, &self.block_tx, &self.router, storage_manager)
             }
+        }
+
+        for block_id in self.router.get_all_block_ids() {
+            self.block_tx.send(block_id).unwrap()
         }
     }
 }
+
 
 #[cfg(test)]
 mod hash_join_tests {
@@ -87,24 +96,50 @@ mod hash_join_tests {
     use std::sync::mpsc;
     use std::mem;
     use crate::operator::hash_join::Hash_Join;
+    use crate::router::BlockPoolDestinationRouter;
+    use hustle_execution_test_util as test_util;
+    use crate::operator::Operator;
+    use super::*;
 
     #[test]
     fn test_hash_join() {
         let storage_manager = StorageManager::with_unique_data_directory();
-
+        println!("a");
         let (input_block_tx_1, input_block_rx_1) = mpsc::channel();
         let (input_block_tx_2, input_block_rx_2) = mpsc::channel();
         let (output_block_tx, output_block_rx) = mpsc::channel();
-
-        // input_block_tx_1.send(block_1.id);
-        // input_block_tx_2.send(block_2.id);
-
+        let block_1 = test_util::example_block(&storage_manager);
+        let block_2 = test_util::example_block(&storage_manager);
+        let catalog = Catalog::new();
+        let mut table = test_util::example_table();
+        let mut schema = table.columns.clone();
+        schema.append(&mut table.columns);
+        // router takes output schema as argument
+        let router = BlockPoolDestinationRouter::new(schema);
+        input_block_tx_1.send(block_1.id);
+        input_block_tx_2.send(block_2.id);
         mem::drop(input_block_tx_1);
         mem::drop(input_block_tx_2);
 
-        let hash_join = Box::new(Hash_Join::new(router, input_block_rx_1, input_block_rx_2, output_block_tx, 0,0))
+        let hash_join = Box::new(Hash_Join::new(router, input_block_rx_1, input_block_rx_2, output_block_tx, 1,1));
+        hash_join.execute(&storage_manager, &catalog);
+        let output_block = storage_manager.get_block(output_block_rx.recv().unwrap()).unwrap();
 
-        let output_block_id = output_block_rx.recv().unwrap();
-
+        assert_eq!(output_block.get_row_col(0, 0), block_1.get_row_col(0, 0));
+        assert_eq!(output_block.get_row_col(0, 1), block_1.get_row_col(0, 1));
+        assert_eq!(output_block.get_row_col(0, 2), block_1.get_row_col(0, 2));
+        assert_eq!(output_block.get_row_col(0, 3), block_2.get_row_col(0, 0));
+        assert_eq!(output_block.get_row_col(0, 4), block_2.get_row_col(0, 1));
+        assert_eq!(output_block.get_row_col(0, 5), block_2.get_row_col(0, 2));
+        assert_eq!(output_block.get_row_col(0, 6), None);
+        assert_eq!(output_block.get_row_col(1, 0), block_1.get_row_col(1, 0));
+        assert_eq!(output_block.get_row_col(1, 1), block_1.get_row_col(1, 1));
+        assert_eq!(output_block.get_row_col(1, 2), block_1.get_row_col(1, 2));
+        assert_eq!(output_block.get_row_col(1, 3), block_2.get_row_col(1, 0));
+        assert_eq!(output_block.get_row_col(1, 4), block_2.get_row_col(1, 1));
+        assert_eq!(output_block.get_row_col(1, 5), block_2.get_row_col(1, 2));
+        assert_eq!(output_block.get_row_col(1, 6), None);
+        assert_eq!(output_block.get_row_col(2, 0), None);
     }
+
 }
